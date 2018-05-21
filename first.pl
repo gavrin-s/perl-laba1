d7b5552e2f3a9a4b8e44984449c4c3a7e78ae660
Sergey <gavrin_s@mail.ru>
2018-05-21 16:36:06 +0700

    First commit

---
+++ first.pl
@@ -0,0 +1,33 @@
+use strict;
+use 5.010;
+use utf8;
+
+my $fname = 'access.log';
+my %hash = ();
+
+open(my $fh, '<', $fname) or die "NO NO NO '$fname' $!";
+while (my $line = <$fh>)
+{
+	if ($line =~ /(\d+\.\d+.\d+.\d+)/)
+	{
+		if(exists($hash{ $1 }))
+		{
+			$hash{ $1 }++;
+		}
+		else
+		{
+			$hash{ $1 } = 1;
+		}
+	}
+}
+
+my $i = 0;
+foreach(sort {$hash{$b} <=> $hash{$a}} keys %hash, )
+{
+	print $_,' = ',$hash{$_},"\n";
+	$i++;
+	if ($i >= 10)
+	{
+		last;
+	}
+}
